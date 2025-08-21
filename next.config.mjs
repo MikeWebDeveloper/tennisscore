879cb474a33694e38979a3626a90d128a19ed653
MikeWebdeveloper <michal.latal@yahoo.co.uk>
2025-08-21 12:27:31 +0100

    fix: remove bundle-analyzer reference

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 import createNextIntlPlugin from 'next-intl/plugin'
-import bundleAnalyzer from '@next/bundle-analyzer'
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -223,4 +219,4 @@ const nextConfig = {
   },
 };
 
-export default withBundleAnalyzer(withNextIntl(nextConfig));
+export default withNextIntl(nextConfig);
